fbffadca343679f0d85d80d046fa995a4f9d949a
shin-minhyuk <afs_style@naver.com>
2025-12-11 20:49:40 +0900

    [feat] first commit

---
+++ src/main/java/com/samulparliament_be/SamulparliamentBeApplication.java
@@ -0,0 +1,13 @@
+package com.samulparliament_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamulparliamentBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamulparliamentBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/samulparliament_be/SamulparliamentBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samulparliament_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamulparliamentBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
