016f31f2be837f3f85ca313e544220a06fac5fdb
shin-minhyuk <afs_style@naver.com>
2025-12-12 18:44:50 +0900

    [feat] refresh_token 에러, baseEntity 제거, 생성 만료 추가, notice, schedule Entity 추가

--- src/main/java/com/samulparliament_be/global/users/entity/RefreshToken.java
+++ src/main/java/com/samulparliament_be/global/users/entity/RefreshToken.java
@@ -1,10 +1,9 @@
 package com.samulparliament_be.global.users.entity;
 
-import com.samulparliament_be.global.common.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "refresh_tokens")
@@ -12,16 +11,33 @@ import java.time.LocalDate;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
-public class RefreshToken extends BaseEntity {
+public class RefreshToken {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "author_id")
+    @JoinColumn(name = "user_id")
     private User author;
 
     @Column(columnDefinition = "text", nullable = false)
     private String token;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "expires_at", nullable = false)
+    private LocalDateTime expiresAt;
+
+    public static RefreshToken create(User author, String token, long days) {
+        LocalDateTime now = LocalDateTime.now();
+
+        return RefreshToken.builder()
+                .author(author)
+                .token(token)
+                .createdAt(now)
+                .expiresAt(now.plusDays(days))
+                .build();
+    }
 }
