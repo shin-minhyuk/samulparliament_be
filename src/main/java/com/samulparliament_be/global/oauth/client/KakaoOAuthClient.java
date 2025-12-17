47fbfc268b77f252b6aa54452517de4873b7360f
shin-minhyuk <afs_style@naver.com>
2025-12-17 20:24:55 +0900

    [feat] OAuth 로깅 추가

--- src/main/java/com/samulparliament_be/global/oauth/client/KakaoOAuthClient.java
+++ src/main/java/com/samulparliament_be/global/oauth/client/KakaoOAuthClient.java
@@ -1,21 +1,25 @@
 package com.samulparliament_be.global.oauth.client;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.samulparliament_be.domain.users.dto.AuthProvider;
+import com.samulparliament_be.global.exception.BusinessException;
+import com.samulparliament_be.global.exception.ErrorCode;
 import com.samulparliament_be.global.oauth.dto.KakaoUserInfoResponse;
 import com.samulparliament_be.global.oauth.dto.OAuthUserInfo;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.stereotype.Component;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class KakaoOAuthClient implements OAuthClient {
 
     @Value("${oauth.kakao.client-id}")
@@ -60,13 +64,20 @@ public class KakaoOAuthClient implements OAuthClient {
         HttpEntity<MultiValueMap<String, String>> request =
                 new HttpEntity<>(params, headers);
 
-        ResponseEntity<Map> response = restTemplate.postForEntity(
-                tokenUri,
-                request,
-                Map.class
-        );
+        try {
+            ResponseEntity<Map> response = restTemplate.postForEntity(
+                    tokenUri,
+                    request,
+                    Map.class
+            );
 
-        return (String) response.getBody().get("access_token");
+            String accessToken = (String) response.getBody().get("access_token");
+            log.info("[OAUTH] 카카오 토큰 발급 성공");
+            return accessToken;
+        } catch (RestClientException e) {
+            log.error("[OAUTH] 카카오 토큰 발급 실패 | error={}", e.getMessage());
+            throw new BusinessException(ErrorCode.OAUTH_TOKEN_ERROR);
+        }
     }
 
     /**
@@ -78,19 +89,26 @@ public class KakaoOAuthClient implements OAuthClient {
 
         HttpEntity<Void> request = new HttpEntity<>(headers);
 
-        ResponseEntity<KakaoUserInfoResponse> response = restTemplate.exchange(
-                userInfoUri,
-                HttpMethod.GET,
-                request,
-                KakaoUserInfoResponse.class
-        );
-        
-        KakaoUserInfoResponse body = response.getBody();
+        try {
+            ResponseEntity<KakaoUserInfoResponse> response = restTemplate.exchange(
+                    userInfoUri,
+                    HttpMethod.GET,
+                    request,
+                    KakaoUserInfoResponse.class
+            );
+            
+            KakaoUserInfoResponse body = response.getBody();
 
-        String email = body.kakao_account().email();
-        String name = body.kakao_account().profile().nickname();
-        String profileImageUrl = body.kakao_account().profile().profile_image_url();
+            String email = body.kakao_account().email();
+            String name = body.kakao_account().profile().nickname();
+            String profileImageUrl = body.kakao_account().profile().profile_image_url();
 
-        return new OAuthUserInfo(name, email, profileImageUrl);
+            log.info("[OAUTH] 카카오 사용자 정보 조회 성공 | email={}", email);
+            return new OAuthUserInfo(name, email, profileImageUrl);
+        } catch (RestClientException e) {
+            log.error("[OAUTH] 카카오 사용자 정보 조회 실패 | error={}", e.getMessage());
+            throw new BusinessException(ErrorCode.OAUTH_PROVIDER_ERROR);
+        }
     }
 }
+
